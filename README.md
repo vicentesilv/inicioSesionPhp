e03d8cd4e6ca7cc18b8b746aa2d4559bd718aaad
vicentesilv <vicentesilvala@gmail.com>
2025-02-05 21:39:09 -0700

    inicio de proyecto de autenticacion por medio de php

---
+++ README.md
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<body>
+    <h1>
+        aplicacion de autenticacion con php
+    </h1>
+    <p>este proyecto es ideal para reforzar conceptos en php, conceptos tales como la autentificacion, la generacion de tokens de acceso con limite de tiempo de expiracion</p>
+    
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vicentesilv/practica-auth-php',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phroute/phroute' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => 'c1893b885966a0c7b50b9239dd867fda7a312dfa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phroute/phroute',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vicentesilv/practica-auth-php' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
